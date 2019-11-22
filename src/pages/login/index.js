a02e4084fc95d667074a96b0482dabf883a3aa2e
wxh1018 <1526859282@qq.com>
2019-11-22 20:19:34 +0800

    login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'dva'
 import Link from 'umi/link'
-import style from './index.css'
+import style from './login.css'
 
 function Login(props) {
     let value = ''
@@ -9,20 +9,27 @@ function Login(props) {
     return (
         <div className={style.box}>
             <div className={style.form}>
-                <label htmlFor="name" >
+                {/* <label htmlFor="name" >
                     <span>账号</span>
                     <input type="text" id='name' ref={(val) => value = val} /></label><br/>
                 <label htmlFor="">
                     <span>密码</span>
                     <input type='password' id='pwd' ref={(pwd) => password = pwd} />
+                </label><br/> */}
+                 <label htmlFor="name" >
+                    <span>账号</span>
+                    <input type="text" id='name' defaultValue='admin'/></label><br/>
+                <label htmlFor="">
+                    <span>密码</span>
+                    <input type='password' id='pwd' value="admin" />
                 </label><br/>
-                <button onClick={() => props.dispatch({
+                <Link to="/index/home"><button onClick={() => props.dispatch({
                     type: 'login/tologin',
                     payload: {
-                        userName: value.value,
-                        password: password.value
+                        userName: 'admin',
+                        password: 'admin'
                     }
-                })}><Link style={{pointerEvents:'none'}} to="/index/home">登录</Link></button>
+                })}>登录</button></Link>
             </div>
         </div>
     );
