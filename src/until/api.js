1a25aac9660c759b1cfe0b09f09cafc490f6b8fd
wxh1018 <1526859282@qq.com>
2019-11-22 13:59:56 +0800

    test

--- src/until/api.js
+++ src/until/api.js
@@ -7,3 +7,7 @@ export const loginapi = (params)=>{
 export const userRegist = (params)=>{
     return API.POST('/api/v1/auth/reg',params)
 }
+// 1. 获取商品列表
+export const getproducts = (params)=>{
+    return API.GET('/api/v1/admin/products',params)
+}
